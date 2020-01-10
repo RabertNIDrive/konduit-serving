b05b9ca349964a17e6b75c3c9587661a8976b5e0
RabertNIDrive <rabert@nidrive.jp>
2020-01-10 18:44:00 +0900

    Schema type added for the python variables BOOL,STR,INT,FLOAT,LIST.

--- konduit-serving-api/src/main/java/ai/konduit/serving/pipeline/step/PythonStep.java
+++ konduit-serving-api/src/main/java/ai/konduit/serving/pipeline/step/PythonStep.java
@@ -119,16 +119,16 @@ public class PythonStep extends BasePipelineStep<PythonStep> {
         try {
             switch (pythonVarType) {
                 case BOOL:
-                    return ai.konduit.serving.config.SchemaType.Boolean;
+                    return SchemaType.Boolean;
                 case STR:
-                    return ai.konduit.serving.config.SchemaType.String;
+                    return SchemaType.String;
                 case INT:
-                    return ai.konduit.serving.config.SchemaType.Integer;
+                    return SchemaType.Integer;
                 case FLOAT:
-                    return ai.konduit.serving.config.SchemaType.Float;
+                    return SchemaType.Float;
                 case NDARRAY:
-                    return ai.konduit.serving.config.SchemaType.NDArray;
                 case LIST:
+                    return SchemaType.NDArray;
                 case FILE:
                 case DICT:
                 default:
@@ -168,6 +168,7 @@ public class PythonStep extends BasePipelineStep<PythonStep> {
                 Output.DataFormat.JSON
         };
     }
+
 
     /**
      * Define a single, named step for a Python pipeline.
@@ -307,6 +308,7 @@ public class PythonStep extends BasePipelineStep<PythonStep> {
 
         return this;
     }
+
 
     @Override
     public String pipelineStepClazz() {
