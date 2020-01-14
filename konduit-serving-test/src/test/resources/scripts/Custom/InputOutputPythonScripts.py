7a9e7c5366d9a90aa396c11d7daca5d9e4d670b5
venkat-nidrive <venkateswarlu@nidrive.jp>
2020-01-14 12:01:53 +0900

    add boolean to script.

--- konduit-serving-test/src/test/resources/scripts/Custom/InputOutputPythonScripts.py
+++ konduit-serving-test/src/test/resources/scripts/Custom/InputOutputPythonScripts.py
@@ -5,12 +5,13 @@ Created on Sat Dec 21 12:48:58 2019
 if type(inputVar) == int:
     output=inputVar
     print("The variable is int ",output)
-
 elif type(inputVar)==float:
     output=inputVar
     print("The variable is float",output)
-        
 elif type(inputVar) == str:
+    output=inputVar
+    print("The variable is string",output)
+elif type(inputVar) == bool:
     output=inputVar
     print("The variable is string",output)
 elif type(inputVar)==dict:
